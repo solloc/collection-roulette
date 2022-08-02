b60078a682a41e10572453a32543217f4daa19f3
solloc <matthias@maisch.me>
2022-08-02 22:21:21 +0200

    randomization implemented

--- apps/app/src/pictures/pictures.controller.ts
+++ apps/app/src/pictures/pictures.controller.ts
@@ -1,10 +1,39 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, ParseIntPipe, Query, Render, Res } from '@nestjs/common';
+import { PicturesService } from './pictures.service';
+import { Response } from 'express';
 
 @Controller('pictures')
 export class PicturesController {
+
+    constructor(private readonly picturesService : PicturesService) {}
+
     @Get()
-    @Render('pictures.hbs')
-    findAll() {
-        return { content: 'pictures' };
+    // @Render('pictures.hbs')
+    findAll(@Query('orderBy') orderBy: string, @Res() response : Response) {
+
+        if (orderBy && orderBy === 'random') {
+            const randomId = this.picturesService.getRandomIndex();
+            console.log(`order by: ${orderBy} => id: ${randomId}`);
+            return response.redirect(HttpStatus.FOUND, `/pictures/${randomId}`);
+        }
+
+        return response.render(
+            'pictures.hbs',
+            { 
+                randomIndex : this.picturesService.getRandomIndex(),
+                count : this.picturesService.getCount() 
+            }
+        );
+        
+        // return { 
+        //     randomIndex : this.picturesService.getRandomIndex(),
+        //     count : this.picturesService.getCount() 
+        // };
+    }
+
+    @Get(':id')
+    @Render('picture.hbs')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.picturesService.getOne(id);
     }
 }
--- apps/app/src/pictures/pictures.service.ts
+++ apps/app/src/pictures/pictures.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+class Picture {
+    location: string;
+}
+
 @Injectable()
-export class PicturesService {}
+export class PicturesService {
+
+    protected mockData : Picture[] = [
+        { location : '/examples/luka-senica-mvKaZllalAQ-unsplash.jpg' },
+        { location : '/examples/maksym-tymchyk-KMDGu1blGi8-unsplash.jpg' },
+        { location : '/examples/andres-molina-umon_iZ7HIA-unsplash.jpg' }
+    ];
+
+    getAll() : Picture[] {
+        return this.mockData;
+    }
+
+    getOne(index : number) : Picture {
+        return this.mockData[index];
+    }
+
+    getRandomIndex() : number {
+        return Math.floor( Math.random() * this.getCount() );
+    }
+
+    getCount() : number {
+        return this.mockData.length;
+    }
+}
