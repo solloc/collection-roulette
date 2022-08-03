7963ec4425f1f3a27e11d9add790929d7472aca2
solloc <matthias@maisch.me>
2022-08-03 21:26:46 +0200

    admin basics added

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from '../admin/admin.module';
 import { PicturesModule } from '../pictures/pictures.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PicturesModule],
+  imports: [PicturesModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
