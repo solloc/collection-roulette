b426662263e70092fdd3ed0a56ee46dde3c625bc
solloc <matthias@maisch.me>
2022-08-03 21:29:27 +0200

    admin included in application

--- apps/app/src/admin/admin.controller.ts
+++ apps/app/src/admin/admin.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+
+    @Get()
+    @Render('admin.hbs')
+    main() {
+        return;
+    }
+
+}
