2f0ec876d53eb2a5995426bee9eecbd065b509a0
solloc <matthias@maisch.me>
2022-08-04 20:28:27 +0200

    jobs with dummy data

--- apps/app/src/jobs/jobs.service.ts
+++ apps/app/src/jobs/jobs.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
 
+enum JobStatus {
+    created = 'CREATED',
+    started = 'STARTED',
+    completed = 'COMPLETED'
+}
+
+class Job {
+    id: number;
+    type: string;
+    started: Date;
+    completed: Date | void;
+    status: JobStatus;
+}
+
 @Injectable()
-export class JobsService {}
+export class JobsService {
+
+    private jobs : Job[] = [
+        {
+            id: 1, 
+            type: 'reindex', 
+            started: new Date('2022-08-04T01:01:01'), 
+            completed: new Date('2022-08-04T02:02:02'),
+            status: JobStatus.completed 
+        }, 
+        {
+            id: 2,
+            type: 'reindex',
+            started: new Date('2022-08-04T03:03:03'),
+            completed: null,
+            status: JobStatus.started
+        }        
+    ]
+
+    findAll(): Job[] {
+        return this.jobs;
+    }
+}
